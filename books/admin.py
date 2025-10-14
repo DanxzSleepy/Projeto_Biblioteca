9d315044abb9b448d37f186c4c18fda4ef914b17
DanxzSleepy <wolfdangalaxy2@gmail.com>
2025-10-14 17:06:19 -0300

    v1.4.1: Added missing profile template and admin configuration

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import Author, Book, Member, BorrowRecord
 
-# Register your models here.
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'birth_date')
+    search_fields = ('first_name', 'last_name')
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'isbn', 'pages', 'available')
+    list_filter = ('available', 'author')
+    search_fields = ('title', 'isbn')
+
+@admin.register(Member)
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ('user', 'phone_number', 'membership_date', 'is_active', 'role')
+    list_filter = ('is_active', 'role')
+    search_fields = ('user__username', 'user__first_name', 'user__last_name')
+
+@admin.register(BorrowRecord)
+class BorrowRecordAdmin(admin.ModelAdmin):
+    list_display = ('book', 'member', 'borrow_date', 'return_date', 'is_returned')
+    list_filter = ('borrow_date', 'return_date')
+    search_fields = ('book__title', 'member__user__username')
+    
+    def is_returned(self, obj):
+        return obj.return_date is not None
+    is_returned.boolean = True
+    is_returned.short_description = 'Devolvido'
